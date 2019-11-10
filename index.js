e293ee61d2a965531a028696b48aa2cf9f5f94d6
GQSM <ms314006@hotmail.com>
2019-11-10 20:49:17 +0800

    [ADD] 把程式放到 Heroku 上，並在 url 後加上 Medium 的文章網址來得到轉換結果

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.setHeader('Content-Type', 'text/html;charset=UTF-8');
   const printRes = (content) => {
-    res.end(content);
+    res.end(`<xmp>${content}</xmp>`);
   };
-  const mediumArticleUrl = 'https://medium.com/enjoy-life-enjoy-coding/react-redux-%E5%B0%8F%E5%AD%A9%E5%AD%90%E6%89%8D%E5%81%9A%E9%81%B8%E6%93%87-hooks-%E5%92%8C-redux-%E6%88%91%E5%85%A8%E9%83%BD%E8%A6%81-1fdd226f5d99';
+
+  if (req.url.indexOf('/getMarkdownFrom') !== -1) {
+    const getMediumUrl = url => url.slice(url.indexOf('/getMarkdownFrom') + 17);
+    const mediumArticleUrl = getMediumUrl(req.url);
+    request(mediumArticleUrl, async (error, response, body) => {
+      const $ = cheerio.load(body);
+      const markdown = new Markdown($);
+      markdown.getArticleContent(printRes);
+    });
+  } else if(req.url === '/favicon.ico') {
+    res.end();
+  } else {
+    res.end('Hi！請在網址上輸入「/getMarkdownFrom/Medium文章網址」進行解析！');
+  }
 });
 
-server.listen(port, () => console.log(`Listening on ${PORT}`));
+server.listen(port, () => console.log(`Listening on ${port}`));
